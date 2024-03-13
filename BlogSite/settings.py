a45253547466da5ce32ac0502db6b040fd8d5d7a
matthias22m <101549294+matthias22m@users.noreply.github.com>
2024-03-13 15:58:46 +0300

    Update settings.py

--- BlogSite/settings.py
+++ BlogSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g*6leo#-05r@+6gxly%4(t!kdxso*ylp59a4gt%trt8h#%h)g4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
